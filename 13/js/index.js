f258a3f5c65e3a4054ed61d7d882cadf256dff96
vi7ali <vi7ali@gmail.com>
2022-06-09 21:08:44 +0300

    finish tabs and accordion

--- 13/js/index.js
+++ 13/js/index.js
@@ -1,5 +1,6 @@
 'use strict';
-console.log('test');
+
+// Swiper
 
 const swiper = new Swiper('.swiper', {
   loop: true,
@@ -9,3 +10,38 @@ const swiper = new Swiper('.swiper', {
     clickable: true,
   },
 });
+
+// How tabs
+
+const tabsButtons = document.querySelectorAll('.how__link');
+const tabsTexts = document.querySelectorAll('.how__content--left');
+const tabsImgs = document.querySelectorAll('.how__content--right');
+
+const showTab = (e) => {
+  const active = e.target.dataset.path;
+  const activeBtn = document.querySelector(`[data-path='${active}']`);
+  const activeText = document.querySelector(`[data-target='text-${active}']`);
+  const activeImg = document.querySelector(`[data-img='img-${active}']`);
+
+  tabsButtons.forEach(btn => btn.classList.remove('how__link--active'));
+  tabsTexts.forEach(text => text.classList.remove('how--active'));
+  tabsImgs.forEach(img => img.classList.remove('how--active'));
+
+  activeBtn.classList.add('how__link--active');
+  activeText.classList.add('how--active');
+  activeImg.classList.add('how--active');
+}
+
+tabsButtons.forEach(tabBtn => {
+  tabBtn.addEventListener('click', showTab);
+});
+
+// Accordion
+
+$(".accordion").accordion({
+  heightStyle: "content",
+  active: false,
+  icons: false,
+  collapsible: true,
+});
+
