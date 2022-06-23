035323f93780de9417bca1a35514b7351231e493
vi7ali <vi7ali@gmail.com>
2022-06-23 21:13:49 +0300

    Finish Yandex map

--- 16/js/index.js
+++ 16/js/index.js
@@ -1,5 +1,5 @@
 const select = document.querySelector("#materialCustom");
-console.log(select);
+// Select
 
 const choices = new Choices(select, {
   placeholder: true,
@@ -7,3 +7,31 @@ const choices = new Choices(select, {
   searchEnabled: false,
   position: 'bottom',
 });
+
+// Map
+
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map("map", {
+      center: [48.872185, 2.354224],
+      zoom: 15,
+  });
+
+  myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/place.svg',
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+  myMap.options.set('suppressMapOpenBlock', true);
+  myMap.controls.remove('zoomControl');
+  myMap.controls.remove('trafficControl');
+  myMap.controls.remove('typeSelector');
+  myMap.controls.remove('fullscreenControl');
+  myMap.controls.remove('searchControl');
+  myMap.controls.remove('geolocationControl');
+  myMap.controls.remove('rulerControl');
+}
+
+
